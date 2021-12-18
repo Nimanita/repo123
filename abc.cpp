83aaeb1e727ce25fc8d4a05a97a3490320fca021
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-12-18 17:06:19 +0530

    Update abc.cpp

--- abc.cpp
+++ abc.cpp
@@ -1,1 +1,9 @@
 as
+weight  
+   password = "3yu"
+   aadhar number = user.aadhar_number
+   userHeight.setText(height + " cm");
+                    userWeight.setText(weight + " kg");
+                    userAge.setText(age + "");
+                    userGender.setText(gender);
+                    userActivityLvl.setText(getActivityLvlShortForm(object.getString("activityLvl")));
