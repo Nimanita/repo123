5e91dc001f93804666f13676715e7bfa0b8880b5
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-12-18 17:08:24 +0530

    Update abc.cpp

--- abc.cpp
+++ abc.cpp
@@ -15,3 +15,15 @@ Height
                     userWeight.setText(weight + " kg");
                     userAge.setText(age + "");
                     userGender.setText(gender);
+Phone Number
+     const email = user.email
+social media password
+const username = "asfhuwneferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="sergueybrp";
+const stripeAcct = 121233;
+nofun
+const phoneNumber = "rgekjrng kjrgunjke";
+const salary ="sergeur";
+illness records = "edyedu"
+const stripeAcct = 121268;
